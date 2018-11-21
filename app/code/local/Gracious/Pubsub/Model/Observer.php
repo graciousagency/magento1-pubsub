84d8cbc6dbf87a3b0168b67a5d02411d8c970fc9
Pepijn Blom <p.blom@gracious.nl>
2018-11-21 10:35:29 +0100

    add basic order support

--- app/code/local/Gracious/Pubsub/Model/Observer.php
+++ app/code/local/Gracious/Pubsub/Model/Observer.php
@@ -1,5 +1,8 @@
 <?php
 
+use Google\Cloud\PubSub\PubSubClient;
+use Google\Cloud\Storage\StorageClient;
+
 /**
  * Created by PhpStorm.
  * User: pepijnblom
@@ -9,12 +12,32 @@
 class Gracious_Pubsub_Model_Observer
 {
 
+    private $pubSub;
+    private $topic;
+
+    public function __construct()
+    {
+
+        Mage::log(__METHOD__ . ' @ ' . __LINE__, null, 'gracious.log');
+        $ordersTopic = Mage::getStoreConfig('pubsub/pubsub_default/orders_topic');
+        $serviceAccount = Mage::getStoreConfig('pubsub/pubsub_default/service_account');
+        Mage::log(__METHOD__ . ' @ ' . __LINE__ . ' -- $ordersTopic = ' . $ordersTopic, null, 'gracious.log');
+        Mage::log(__METHOD__ . ' @ ' . __LINE__ . ' -- $serviceAccount = ' . $serviceAccount, null, 'gracious.log');
+        $this->pubSub = new PubSubClient(
+            ['keyFile' => json_decode($serviceAccount, true)]
+        );
+        $this->topic = $this->pubSub->topic($ordersTopic);
+
+    }
+
     public function salesOrderPlaceAfter($observer)
     {
+
         Mage::log(__METHOD__ . ' @ ' . __LINE__ . ' -- $observer = ' . get_class($observer), null, 'gracious.log');
         $order = $observer->getEvent()->getOrder();
-        Zend_Debug::dump($order);
-        die;
+        $message = ['data' => json_encode($order->getData()), 'attributes' => ['type' => 'order']];
+        $result = $this->topic->publish($message);
+        Mage::log(__METHOD__ . ' @ ' . __LINE__ . ' -- $result = ' . print_r($result,true), null, 'gracious.log');
     }
 
 }
